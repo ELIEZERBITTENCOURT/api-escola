f6a217fe1d38a7cbd22aaf8c6c191fd15e277257
Eliezer Bittencourt <likobittencourt@gmail.com>
2024-03-14 19:45:35 -0300

    test add file test

--- setup/settings.py
+++ setup/settings.py
@@ -142,13 +142,21 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
-    }  
+    },
+     'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework_xml.parsers.XMLParser',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
+    ],
 }
 
 # CORS
-CORS_ALLOWED_ORIGINS = [
-    "http:localhost:3000",
-]
+#CORS_ALLOWED_ORIGINS = [
+#   "http:localhost:3000",
+#]
 
 CACHES = {
     "default":{
